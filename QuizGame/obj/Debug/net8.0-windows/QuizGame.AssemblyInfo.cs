dee02b538661a98075779f66ca8eb7e840ced6d0
Jorma Lindh <jorma.lindh@mimirsbrunnr.cloud>
2025-11-10 11:40:08 +0100

    Renamed Dota 2 JSON so that it is loaded properly

--- QuizGame/obj/Debug/net8.0-windows/QuizGame.AssemblyInfo.cs
+++ QuizGame/obj/Debug/net8.0-windows/QuizGame.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuizGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64ed45657e68384ea028572da16c5c03c46e1be1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2262d559bb2c261549a5654b407e09fcfe009efb")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuizGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuizGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
